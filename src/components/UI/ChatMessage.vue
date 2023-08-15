ea69e4f8302a5bababba03d0d6bbb21b2bb029a8
fengzhiqi <46121429+qizhifeng00@users.noreply.github.com>
2023-08-15 12:44:18 +0800

    ASDASD

--- src/components/UI/ChatMessage.vue
+++ src/components/UI/ChatMessage.vue
@@ -1,15 +1,16 @@
 <template>
-  <q-chat-message
-    :text="[text]"
-    :sent="sent"
-  />
+  <q-chat-message :text="[text]" :sent="sent" />
+  1
 </template>
 <script setup lang="ts">
-withDefaults(defineProps<{
-  text: string,
-  sent?: boolean
-}>(), {
-  text: '',
-  sent: false
-})
+withDefaults(
+  defineProps<{
+    text: string;
+    sent?: boolean;
+  }>(),
+  {
+    text: '',
+    sent: false,
+  }
+);
 </script>
